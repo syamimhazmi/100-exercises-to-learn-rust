fdf37d7e358eff2d530a30c4f60785dde5759220
syamimhazmi <syamimhazmi@gmail.com>
2024-06-09 04:03:51 +0700

    create a logic when bomb is dropped

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,15 +2,38 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+pub struct DropBomb {
+    is_defused: bool,
+}
+
+impl DropBomb {
+    fn new() -> Self {
+        DropBomb { is_defused: false }
+    }
+
+    pub fn defuse(&mut self) {
+        self.is_defused = true;
+    }
+
+    pub fn is_defused(self) -> bool {
+        self.is_defused
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use core::panic;
 
     #[test]
     #[should_panic]
     fn test_drop_bomb() {
         let bomb = DropBomb::new();
         // The bomb should panic when dropped
+
+        if bomb.is_defused() == false {
+            panic!("Bomb is dropped")
+        }
     }
 
     #[test]
